9df15f018cec8591a9e03d5c9381065848713b16
MinToan35 <mintoan351998@gmail.com>
2022-08-16 13:18:14 +0700

    mongo

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern.y8kzf.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
 app.use(express.json());
 app.use(cors());
